4f96125e5dc3a33dfafe251d09050f0e86e79838
IlyaBaburashvili <48105767+IlyaBaburashvili@users.noreply.github.com>
2022-10-05 01:43:29 -0400

    a

--- SentimentAnalysis/SentimentAnalysis.py
+++ SentimentAnalysis/SentimentAnalysis.py
@@ -14,10 +14,13 @@ nltk.download('stopwords')
 positive_tweets_training_set = twitter_samples.strings('positive_tweets.json')
 negative_tweets_training_set = twitter_samples.strings('negative_tweets.json')
 neutral_tweets_training_set = []
+full_training_set = {}
 sentiments = ['negative', 'positive', 'neutral']
 sentiment_frequencies = {}
+total_words = total_negative = total_unique_negative = total_positive = total_unique_positive = total_neutral = total_unique_neutral = 0
 
 def preprocess(tweet):
+    tweet = re.sub(r'[^\x00-\x7F]', '', tweet) 
     tweet = re.sub(r'[^a-zA-Z0-9]', '', tweet)
     tweet = re.sub(r'[0-9]+', '', tweet)
     tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
@@ -44,8 +47,40 @@ def update_sentiment_count(tweet, sentiment):
         if word in sentiment_frequencies:
             sentiment_frequencies[word][sentiment]+=1
         else:
-            sentiment_frequencies[word] = [0, 0, 0]
+            sentiment_frequencies[word] = [1, 1, 1]  #all start with 1 to avoid probability of 0
             sentiment_frequencies[word][sentiment]+=1
 
+#def get_all_sentiments(positive_tweets, negative_tweets, neutral_tweets):
+ #   all_sentiments = np.array([])
+  #  all_sentiments = np.append(all_sentiments, np.ones(len(positive_tweets)))
+  #  all_sentiments = np.append(all_sentiments, np.zeros(len(negative_tweets)))
+  #  all_sentiments = np.append(all_sentiments, 2(len(neutral_tweets)))
+  #  return all_sentiments
+
+def create_full_training_set(positive_training_set, negative_training_set, neutral_training_set):
+    for tweet in negative_training_set:
+        full_training_set[tweet] = 0
+    for tweet in positive_training_set:
+        full_training_set[tweet] = 1
+    for tweet in neutral_training_set:
+        full_training_set[tweet] = 2
+
+def get_totals(sentiment_frequencies):
+    total_unique_words = len(sentiment_frequencies)
+    #total_unique_negative = 0
+    #total_unique_positive = 0
+    #total_unique_neutral = 0
+    total_words_in_positive = 0
+    total_words_in_negative = 0
+    total_words_in_neutral = 0
+    for word in sentiment_frequencies:
+        total_words_in_negative+=sentiment_frequencies[word][0]
+        total_words_in_positive+=sentiment_frequencies[word][1]
+        total_words_in_neutral+=sentiment_frequencies[word][2]
+    return total_unique_words, total_words_in_negative, total_unique_positive, total_words_in_neutral
 
 
+def train_model(sentiment_frequencies, full_training_set_clean):
+    probability_positive = len(positive_tweets_training_set)/len(full_training_set_clean)
+    probability_negative = len(negative_tweets_training_set)/len(full_training_set_clean)
+    probability_neutral = len(neutral_tweets_training_set)/len(full_training_set_clean)
